da6d508fa779a92e93907c3c48d4e403bcec5191
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-26 15:29:04 -0300

    Entidade Product

---
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,117 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+
+	/*
+	 * Na associação, será usado o Set. Porque representa um conjunto, para garantir
+	 * que não vou ter um product com mais de uma ocorrencia da mesma category.
+	 * 
+	 * O set será instanciado para que a coleção não comece valendo nula. Pode
+	 * começar vazia, porém instanciada. Usou-se o hashSet ao inves de Set, pq o set
+	 * é uma interface e por isso não é instanciada.
+	 * 
+	 * "Dentro do Product, tem-se um conjunto de Category"
+	 * 
+	 */
+	@Transient
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+
+	}
+
+	// Não coloca coleções no Construtor. Pq ela já está sendo instanciada.
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
