4399e82ab4572e0040253ec8c49892bb10e154fc
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-26 09:00:29 -0300

    Entidade Order, Datas com Instant e padrão ISO 8601 - parte 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity // Para instruir o JPA, passar de POO para relacional, ao conectar ao banco
@@ -21,6 +24,17 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	/*
+	 * Mapeamento de OneToMany é opcional: É para acessar um obj do tipo usuário e
+	 * acessar automaticamente os pedidos (ORDER) feitos por esse usuário. Mas é
+	 * preciso colocar o nome do atributo que ta do outro lado da associação,
+	 * nesse caso, o outro lado da associação é ORDER, e o nome do atributo é
+	 * "client". mappedby = mapeado por "cliente".
+	 */
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>(); // É uma associaçaõ e uma coleção, devido isso faz-se soemnte os
+													// get.
 
 	/*
 	 * Após o mapeamento "@Entity...@Id..." Pode-se acessar
@@ -78,6 +92,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
