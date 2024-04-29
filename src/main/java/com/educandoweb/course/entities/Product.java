cf747dd5f046dc10fc86b2046036b595a2e4caed
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-04-29 13:34:09 -0300

    Associação ManyToMany entre Product e OrderItem

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,6 +44,9 @@ public class Product implements Serializable {
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 
@@ -99,6 +105,20 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+
+	/*
+	 * getOrders() = mesmo nome projetado no diagrama (- orders). É necessário
+	 * varrer os orderItem, e para cada orderItem será pego o order associado ao
+	 * orderItem.
+	 */
+	@JsonIgnore
+	public Set<Order> getOrders() {
+		Set<Order> set = new HashSet<>();
+		for (OrderItem x : items) { // Percorrendo cada objeto do tipo OrderItem contido na lista de "items"
+			set.add(x.getOrder());// Vai ser adicionado no set
+		}
+		return set; // Apos fazer isso com todos, será retornado o set
 	}
 
 	@Override
